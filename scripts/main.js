eec990531e6b4db216c4cefd07eb8b20205968e7
Kpred <Kpred1@hotmail.com>
2021-03-21 00:03:13 -0500

    initial

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+let myImage = document.querySelector('img');
+
+
+
+let myHeading = document.querySelector('h1');
+function setUserName() {
+let myname=null ;
+  
+  myHeading.textContent = 'Why you should date me '
+}
+
+  if(!localStorage.getItem('name')) {
+    setUserName(); } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = ' Why you should date me '
+  }
+  
+
+  
+  
